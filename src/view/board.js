77c0d54377d71b3d77f23b274d4ae2daff4f81dd
Radja7 <Radja7@users.noreply.github.com>
2021-09-30 23:55:28 +0300

    reset

--- src/view/board.js
+++
@@ -1,9 +0,0 @@
-import AbstractView from './abstract.js';
-
-const createBoardTemplate = () => '<section class="board container"></section>';
-
-export default class Board extends AbstractView {
-  getTemplate() {
-    return createBoardTemplate();
-  }
-}
