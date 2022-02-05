de32b2f73b3fe716fb0c833edaccbebe7a064c42
Radja7 <Radja7@users.noreply.github.com>
2022-02-05 22:37:04 +0300

    Components into classes

--- src/components/site-menu.js
+++ src/components/site-menu.js
@@ -1,5 +1,8 @@
-export const createSiteMenuTemplate = () => (
-  `<section class="control__btn-wrap">
+import {createElement} from '../utils';
+
+const createSiteMenuTemplate = () => {
+  return (
+    `<section class="control__btn-wrap">
           <input
             type="radio"
             name="control"
@@ -27,4 +30,31 @@ export const createSiteMenuTemplate = () => (
             >STATISTICS</label
           >
      </section>`
-);
+  );
+};
+
+export default class SiteMenu {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSiteMenuTemplate();
+  }
+
+  getElement() {
+    if(!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+
+
+
