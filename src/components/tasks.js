99392e791e5ddc83da735cf940d134a1d53299c2
Radja7 <Radja7@users.noreply.github.com>
2022-02-05 22:48:38 +0300

    fix board

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,22 +1,22 @@
 import {createElement} from '../utils';
 
-const createBoardTemplate = () => {
+const createTasksTemplate = () => {
   return (
-    `<section class="board container"></section>`
+    `<div class="board__tasks"></div>`
   );
 };
 
-export default class Board {
+export default class Tasks {
   constructor() {
     this._element = null;
   }
 
   getTemplate() {
-    return createBoardTemplate();
+    return createTasksTemplate();
   }
 
   getElement() {
-    if (!this._element) {
+    if(!this._element) {
       this._element = createElement(this.getTemplate());
     }
 
