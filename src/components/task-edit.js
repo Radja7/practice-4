a212ba11fbbb09523637f0c5254a3331d006c3f5
Radja7 <Radja7@users.noreply.github.com>
2022-02-13 14:52:41 +0300

    Components extends abstract component

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -1,5 +1,6 @@
 import {COLORS, DAYS, MONTH_NAMES} from '../const';
-import {createElement, formatTime} from '../utils';
+import AbstractComponent from '../abstract-component.js';
+import {formatTime} from '../utils';
 
 const createColorsMarkup = (colors, currentColor) => (
   colors
@@ -119,25 +120,14 @@ const createTaskEditTemplate = (task) => {
      </article>`;
 };
 
-export default class TaskEdit {
+export default class TaskEdit extends AbstractComponent {
   constructor(task) {
+    super();
+
     this._task = task;
-    this._element = null;
   }
 
   getTemplate() {
     return createTaskEditTemplate(this._task);
-  }
-
-  getElement() {
-    if(!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
