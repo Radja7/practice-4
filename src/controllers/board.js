d392878d123af0dd47f0ddd78bc7be8e644bf9c2
Radja7 <Radja7@users.noreply.github.com>
2022-05-10 17:18:35 +0300

    Reconfiguring the board controller to work with a child controller

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -6,12 +6,17 @@ import SortComponent, {SortType} from '../components/sort.js';
 import {render, remove, RenderPosition} from "../utils/render.js";
 
 
+
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const renderTasks = (taskListElement, tasks) => {
-  tasks.forEach((task) => {
-    renderTask(taskListElement, task);
+  return tasks.map((task) => {
+    const taskController = new TaskController(taskListElement);
+
+    taskController.render(task);
+
+    return taskController;
   });
 };
 
@@ -39,6 +44,7 @@ export default class BoardController {
     this._container = container;
 
     this._tasks = [];
+    this._showedTaskControllers = [];
     this._showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
     this._noTasksComponent = new NoTasksComponent();
     this._sortComponent = new SortComponent();
@@ -66,7 +72,8 @@ export default class BoardController {
 
     const taskListElement = this._tasksComponent.getElement();
 
-    renderTasks(taskListElement, tasks.slice(0, this._showingTasksCount));
+    const newTasks = renderTasks(taskListElement, this._tasks.slice(0, this._showingTasksCount));
+    this._showedTaskControllers = this._showedTaskControllers.concat(newTasks);
 
     this._renderLoadMoreButton();
   }
@@ -84,26 +91,30 @@ export default class BoardController {
       const taskListElement = this._tasksComponent.getElement();
       this._showingTasksCount = this._showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
 
-      //const sortedTasks = getSortedTasks(tasks, this._sortComponent.getSortType(), prevTasksCount, this._showingTasksCount);
+      const sortedTasks = getSortedTasks(this._tasks, this._sortComponent.getSortType(), prevTasksCount, this._showingTasksCount);
+      const newTasks = renderTasks(taskListElement, sortedTasks);
 
-      //renderTasks(taskListElement, sortedTasks);
+      this._showedTaskControllers = this._showedTaskControllers.concat(newTasks);
 
-      renderTasks(taskListElement, sortedTasks.slice(0, this._showingTasksCount));
-
-      // if(this._showingTasksCount >= this._tasks.length) {
-      //   remove(this._loadMoreButtonComponent);
-      // }
+      if(this._showingTasksCount >= this._tasks.length) {
+        remove(this._loadMoreButtonComponent);
+      }
     });
   };
 
   _onSortTypeChange(sortType) {
-    showingTasksCount = SHOWING_TASKS_COUNT_BY_BUTTON;
+   showingTasksCount = SHOWING_TASKS_COUNT_BY_BUTTON;
 
     const sortedTasks = getSortedTasks(tasks, sortType, 0, showingTasksCount);
 
     taskListElement.innerHTML = '';
 
-    renderTasks(taskListElement, sortedTasks);
+    //renderTasks(taskListElement, sortedTasks);
+    const newTasks = renderTasks(taskListElement, sortedTasks);
+    this._showedTaskControllers = newTasks;
+
     this._renderLoadMoreButton();
   }
 }
+
+
